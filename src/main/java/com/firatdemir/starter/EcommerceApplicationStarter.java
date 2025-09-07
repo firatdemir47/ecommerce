ee821bd4ded8ecd361cde43e54d3ca4e678206c1
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-07 14:32:38 +0300

    Initial commit: Spring Boot ecommerce project setup

---
+++ src/main/java/com/firatdemir/starter/EcommerceApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.firatdemir.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplicationStarter.class, args);
+	}
+
+}
