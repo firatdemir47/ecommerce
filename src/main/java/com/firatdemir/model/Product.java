9ef22d2f968c7785e10775ba05249c65fe8543c6
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-07 15:32:32 +0300

    Add Product entity with id, name, description, price, stock, and imageUrl fields

---
+++ src/main/java/com/firatdemir/model/Product.java
@@ -0,0 +1,37 @@
+package com.firatdemir.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	private String description;
+
+	@Column(nullable = false)
+	private Double price;
+
+	@Column(nullable = false)
+	private Integer stock;
+
+	private String imageUrl;
+
+}
