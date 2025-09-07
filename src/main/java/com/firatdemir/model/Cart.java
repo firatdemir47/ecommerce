f67488751e3f571fac286f5117ac81de446fbab7
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-07 15:51:34 +0300

    Add one-to-many relationship from Cart to CartItem

--- src/main/java/com/firatdemir/model/Cart.java
+++ src/main/java/com/firatdemir/model/Cart.java
@@ -1,10 +1,14 @@
 package com.firatdemir.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,13 +21,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cart {
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@OneToOne
 	@JoinColumn(name = "user_id")
 	private User user;
-	
-	
+
+	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+	private List<CartItem> items;
+
 }
