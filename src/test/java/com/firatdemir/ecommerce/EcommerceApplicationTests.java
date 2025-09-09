92ea827a871a691b7300d05a873c516bc11b1309
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-09 11:45:26 +0300

    Fix test configuration and CI/CD issues

--- src/test/java/com/firatdemir/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/firatdemir/ecommerce/EcommerceApplicationTests.java
@@ -2,10 +2,10 @@ package com.firatdemir.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = com.firatdemir.starter.EcommerceApplicationStarter.class)
+@ActiveProfiles("test")
 class EcommerceApplicationTests {
 
 	@Test
