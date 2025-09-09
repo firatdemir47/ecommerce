e50d1c9dae79f046accc4270c06364d426c0ba00
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-09 13:29:05 +0300

    feat: Add CategoryDto class with id, name, and productsIds fields

---
+++ src/main/java/com/firatdemir/dto/CategoryDto.java
@@ -0,0 +1,21 @@
+package com.firatdemir.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+
+	private Long id;
+
+	private String name;
+
+	private List<Long> productsIds;
+}
