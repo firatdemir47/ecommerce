6e0f9276369302a597b718bf50eaea2fa51430bc
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-09 13:34:43 +0300

    feat: Add CartItemDto class with id, cartId, productId, and quantity fields

---
+++ src/main/java/com/firatdemir/dto/CarItemDto.java
@@ -0,0 +1,21 @@
+package com.firatdemir.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarItemDto {
+	
+	private Long id;
+	
+	private Long cartid;
+	
+	private Long productId;
+	
+	private Integer quantity;
+}
