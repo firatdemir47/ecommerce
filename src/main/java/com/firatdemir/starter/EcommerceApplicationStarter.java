2c5364826e08f0200a084151fa17ea4a3760d27b
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-09 11:41:07 +0300

    Fix component scanning for Spring Boot tests

--- src/main/java/com/firatdemir/starter/EcommerceApplicationStarter.java
+++ src/main/java/com/firatdemir/starter/EcommerceApplicationStarter.java
@@ -3,11 +3,13 @@ package com.firatdemir.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.firatdemir"})
 @EnableJpaRepositories(basePackages = {"com.firatdemir"})
+@ComponentScan(basePackages = {"com.firatdemir"})
 public class EcommerceApplicationStarter {
 
 	public static void main(String[] args) {
