3b921ef95e5fc393dbe3d8bd1875a64471510b50
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-28 17:35:41 +0300

    feat: add toDto mapper in CategoryServiceImpl

--- src/main/java/com/firatdemir/service/ımpl/CategoryServiceImpl.java
+++ src/main/java/com/firatdemir/service/ımpl/CategoryServiceImpl.java
@@ -1,18 +1,21 @@
 package com.firatdemir.service.ımpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.firatdemir.dto.CategoryDto;
+import com.firatdemir.model.Category;
+import com.firatdemir.model.Product;
 import com.firatdemir.service.CategoryService;
 
 @Service
-public class CategoryServiceImpl  implements CategoryService{
+public class CategoryServiceImpl implements CategoryService {
 
 	@Override
 	public CategoryDto creatCategory(CategoryDto dto) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
@@ -37,8 +40,19 @@ public class CategoryServiceImpl  implements CategoryService{
 	@Override
 	public void deleteCategory(Long id) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	private CategoryDto todDto(Category entity) {
+		CategoryDto dto = new CategoryDto();
+		dto.setId(entity.getId());
+		dto.setName(entity.getName());
+
+		if (entity.getProducts() != null) {
+			dto.setProductsIds(entity.getProducts().stream().map(Product::getId).collect(Collectors.toList()));
+
+		}
+		return dto;
+	}
 
 }
