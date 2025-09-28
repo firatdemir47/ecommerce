5ed233bc7293c04eaf5d773ceba21ce1a6aa835b
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-28 17:00:50 +0300

    feat: add CategoryServiceImpl with method stubs

---
+++ src/main/java/com/firatdemir/service/ımpl/CategoryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.firatdemir.service.ımpl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.firatdemir.dto.CategoryDto;
+import com.firatdemir.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl  implements CategoryService{
+
+	@Override
+	public CategoryDto creatCategory(CategoryDto dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoryDto getCategoryByID(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CategoryDto> getAllCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoryDto updaCategory(Long id, CategoryDto dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteCategory(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
