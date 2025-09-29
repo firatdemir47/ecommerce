028a9af3ccec75d08a21311c9481a7ecddcb587b
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-29 15:17:44 +0300

    feat: Add existsByUsername and existsByEmail methods to UserRepository

--- src/main/java/com/firatdemir/repository/UserRepository.java
+++ src/main/java/com/firatdemir/repository/UserRepository.java
@@ -8,4 +8,7 @@ import com.firatdemir.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	boolean existsByUsername(String username);
+
+	boolean existsByEmail(String email);
 }
