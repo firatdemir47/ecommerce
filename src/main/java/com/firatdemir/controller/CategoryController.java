ce4c2a941264435a8b50aeeb1cdb72c4c6c4abeb
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-29 10:42:29 +0300

    feat: Add CategoryController with createCategory endpoint

---
+++ src/main/java/com/firatdemir/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.firatdemir.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firatdemir.dto.CategoryDto;
+import com.firatdemir.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+	private final CategoryService categoryService;
+
+	public CategoryController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto dto) {
+
+		return ResponseEntity.ok(categoryService.creatCategory(dto));
+	}
+}
