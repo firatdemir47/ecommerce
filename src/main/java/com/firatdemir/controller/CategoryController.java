5e14050507f5c35ff5e649643d636ebc565dd07b
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-29 10:48:11 +0300

    feat: Add getCategoryById endpoint

--- src/main/java/com/firatdemir/controller/CategoryController.java
+++ src/main/java/com/firatdemir/controller/CategoryController.java
@@ -1,6 +1,10 @@
 package com.firatdemir.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,10 @@ public class CategoryController {
 	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto dto) {
 
 		return ResponseEntity.ok(categoryService.creatCategory(dto));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoryDto> getCategoryById(@PathVariable Long id) {
+		return ResponseEntity.ok(categoryService.getCategoryByID(id));
 	}
 }
