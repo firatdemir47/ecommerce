700cbddd72671c87eb5e797d842b47714fc1db79
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-30 14:35:01 +0300

    feat: add deleteUser endpoint to UserController

--- src/main/java/com/firatdemir/controller/UserController.java
+++ src/main/java/com/firatdemir/controller/UserController.java
@@ -37,4 +37,10 @@ public class UserController {
 	public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
 		return ResponseEntity.ok(userService.updateUser(id, userDto));
 	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
+		return ResponseEntity.noContent().build();
+	}
 }
