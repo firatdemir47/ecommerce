5bc0525b67b71f9459120258834779a3ef67909a
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-03 16:25:01 +0300

    feat: add repositories constructor to OrderServiceImpl

--- src/main/java/com/firatdemir/service/ımpl/OrderServiceImpl.java
+++ src/main/java/com/firatdemir/service/ımpl/OrderServiceImpl.java
@@ -3,13 +3,37 @@ package com.firatdemir.service.ımpl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
 import com.firatdemir.dto.OrderDto;
 import com.firatdemir.dto.OrderItemDto;
 import com.firatdemir.model.Order;
 import com.firatdemir.model.OrderItem;
+import com.firatdemir.repository.CartItemRepository;
+import com.firatdemir.repository.CartRepository;
+import com.firatdemir.repository.OrderItemRepository;
+import com.firatdemir.repository.OrderRepository;
+import com.firatdemir.repository.UserRepository;
 import com.firatdemir.service.OrderService;
 
+@Service
 public class OrderServiceImpl implements OrderService {
+
+	private final OrderRepository orderRepository;
+	private final OrderItemRepository orderItemRepository;
+	private final UserRepository userRepository;
+	private final CartRepository cartRepository;
+	private final CartItemRepository cartItemRepository;
+
+	public OrderServiceImpl(OrderRepository orderRepository, OrderItemRepository orderItemRepository,
+			UserRepository userRepository, CartRepository cartRepository, CartItemRepository cartItemRepository) {
+
+		this.orderRepository = orderRepository;
+		this.orderItemRepository = orderItemRepository;
+		this.userRepository = userRepository;
+		this.cartRepository = cartRepository;
+		this.cartItemRepository = cartItemRepository;
+	}
 
 	@Override
 	public OrderDto createOrder(Long userId) {
