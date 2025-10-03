e1711cd69d597ca1ae331d36b52e7a2d065f6e48
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-03 16:18:37 +0300

    feat: add toDto methods in OrderServiceImpl

--- src/main/java/com/firatdemir/service/ımpl/OrderServiceImpl.java
+++ src/main/java/com/firatdemir/service/ımpl/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package com.firatdemir.service.ımpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.firatdemir.dto.OrderDto;
 import com.firatdemir.dto.OrderItemDto;
+import com.firatdemir.model.Order;
 import com.firatdemir.model.OrderItem;
 import com.firatdemir.service.OrderService;
 
@@ -31,6 +33,17 @@ public class OrderServiceImpl implements OrderService {
 	public List<OrderDto> getAllOrders() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private OrderDto toDto(Order order) {
+		OrderDto dto = new OrderDto();
+		dto.setId(order.getId());
+		dto.setOrderDate(order.getOrderDate());
+		dto.setUserId(order.getUser().getId());
+		if (order.getItems() != null) {
+			dto.setItems(order.getItems().stream().map(this::toDto).collect(Collectors.toList()));
+		}
+		return dto;
 	}
 
 	private OrderItemDto toDto(OrderItem item) {
