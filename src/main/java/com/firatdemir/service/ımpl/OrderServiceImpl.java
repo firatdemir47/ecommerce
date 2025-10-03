5ecfbf39b79c771482bdc3c4e311d6c236ec5273
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-03 16:06:24 +0300

    feat: add OrderServiceImpl with method stubs

---
+++ src/main/java/com/firatdemir/service/ımpl/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package com.firatdemir.service.ımpl;
+
+import java.util.List;
+
+import com.firatdemir.dto.OrderDto;
+import com.firatdemir.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public OrderDto createOrder(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderDto getOrderById(Long orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderDto> getOrdersByUserId(Long UserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderDto> getAllOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
