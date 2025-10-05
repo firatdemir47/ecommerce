b6eba8aec2c81ad2a75d418a588124be77f52962
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-05 21:44:51 +0300

    feat: add OrderItemServiceImpl class with method stubs

---
+++ src/main/java/com/firatdemir/service/ımpl/OrderItemServiceImpl.java
@@ -0,0 +1,43 @@
+package com.firatdemir.service.ımpl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.firatdemir.dto.OrderItemDto;
+import com.firatdemir.service.OrderItemService;
+
+@Service
+public class OrderItemServiceImpl  implements OrderItemService{
+
+	@Override
+	public OrderItemDto getOrderItemById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderItemDto> getOrderItemsByOrderId(Long orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderItemDto createOrderItem(OrderItemDto orderItemDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderItemDto updateOrderItem(Long id, OrderItemDto orderItemDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteOrderItem(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
