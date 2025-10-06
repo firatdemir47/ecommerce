468aff86859fb41a2e7bfef3538a0513a01b670b
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-06 11:02:14 +0300

    feat: add createOrderItem endpoint to OrderItemController

--- src/main/java/com/firatdemir/controller/OrderItemController.java
+++ src/main/java/com/firatdemir/controller/OrderItemController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +33,8 @@ public class OrderItemController {
 		return ResponseEntity.ok(orderItemService.getOrderItemsByOrderId(orderId));
 	}
 
+	  @PostMapping
+	    public ResponseEntity<OrderItemDto> createOrderItem(@RequestBody OrderItemDto orderItemDto) {
+	        return ResponseEntity.ok(orderItemService.createOrderItem(orderItemDto));
+	    }
 }
