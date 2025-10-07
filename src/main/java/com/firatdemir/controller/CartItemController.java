bbe461d775d906e49434d5a639687bde38ac3c4c
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-07 15:29:59 +0300

    feat: add createCartItem endpoint to CartItemController

--- src/main/java/com/firatdemir/controller/CartItemController.java
+++ src/main/java/com/firatdemir/controller/CartItemController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +32,9 @@ public class CartItemController {
 	public ResponseEntity<List<CartItemDto>> getAllCartItems() {
 		return ResponseEntity.ok(cartItemService.getAllCartItems());
 	}
+	
+	@PostMapping
+    public ResponseEntity<CartItemDto> createCartItem(@RequestBody CartItemDto dto) {
+        return ResponseEntity.ok(cartItemService.createCartItem(dto));
+    }
 }
