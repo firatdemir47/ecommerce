c8f250531bf3504c4704a5dd658da9bf8527036f
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-07 15:17:37 +0300

    feat: inject repositories into CartItemServiceImpl

--- src/main/java/com/firatdemir/service/ımpl/CartItemServiceImpl.java
+++ src/main/java/com/firatdemir/service/ımpl/CartItemServiceImpl.java
@@ -5,10 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.firatdemir.dto.CartItemDto;
+import com.firatdemir.repository.CartItemRepository;
+import com.firatdemir.repository.CartRepository;
+import com.firatdemir.repository.ProductRepository;
 import com.firatdemir.service.CartItemService;
 
 @Service
-public class CartItemServiceImpl  implements CartItemService{
+public class CartItemServiceImpl implements CartItemService {
+
+	private final CartItemRepository cartItemRepository;
+	private final CartRepository cartRepository;
+	private final ProductRepository productRepository;
+
+	public CartItemServiceImpl(CartItemRepository cartItemRepository, CartRepository cartRepository,
+			ProductRepository productRepository) {
+		this.cartItemRepository = cartItemRepository;
+		this.cartRepository = cartRepository;
+		this.productRepository = productRepository;
+	}
 
 	@Override
 	public CartItemDto getCartItemById(Long id) {
@@ -37,7 +51,7 @@ public class CartItemServiceImpl  implements CartItemService{
 	@Override
 	public void deleteCartItem(Long id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
