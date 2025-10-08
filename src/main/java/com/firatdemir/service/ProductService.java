726da0c7352c13d043e780976dc578076683a763
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-08 15:04:08 +0300

    feat(api): add pageable getAllProducts to ProductService

--- src/main/java/com/firatdemir/service/ProductService.java
+++ src/main/java/com/firatdemir/service/ProductService.java
@@ -2,6 +2,9 @@ package com.firatdemir.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.firatdemir.dto.ProductDto;
 
 public interface ProductService {
@@ -11,6 +14,8 @@ public interface ProductService {
 	ProductDto getProductById(Long id);
 
 	List<ProductDto> getAllProducts();
+
+	Page<ProductDto> getAllProducts(Pageable pageable);
 
 	ProductDto updateProduct(Long id, ProductDto productDto);
 
